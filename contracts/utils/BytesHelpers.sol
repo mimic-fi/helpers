927293fc607a2c96a65281583b93351212ee0c04
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2023-06-25 12:38:45 -0300

    Price Oracle: Merge on and off chain oracles (#12)

---
+++ contracts/utils/BytesHelpers.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title BytesHelpers
+ * @dev Provides a list of Bytes helper methods
+ */
+library BytesHelpers {
+    function toUint256(bytes memory self, uint256 offset) internal pure returns (uint256 result) {
+        require(self.length >= offset + 32, 'BYTES_OUT_OF_BOUNDS');
+        assembly {
+            result := mload(add(add(self, 0x20), offset))
+        }
+    }
+}
