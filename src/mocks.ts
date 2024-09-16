13e9fe45467dd35452999b9363f6a3bd3f6f9ff7
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2024-09-16 08:24:36 -0300

    contracts: remove price feed mock

--- src/mocks.ts
+++ src/mocks.ts
@@ -1,7 +1,6 @@
 import { Contract } from 'ethers'
 
 import { deploy } from './contracts'
-import { BigNumberish } from './numbers'
 
 /* eslint-disable no-secrets/no-secrets */
 
@@ -11,8 +10,4 @@ export async function deployWrappedNativeTokenMock(): Promise<Contract> {
 
 export async function deployTokenMock(symbol: string, decimals = 18): Promise<Contract> {
   return deploy('@mimic-fi/helpers/artifacts/contracts/mocks/TokenMock.sol/TokenMock', [symbol, decimals])
-}
-
-export async function deployFeedMock(price: BigNumberish, decimals = 18): Promise<Contract> {
-  return deploy('@mimic-fi/v3-helpers/artifacts/contracts/mocks/FeedMock.sol/FeedMock', [price, decimals])
 }
