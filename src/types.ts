419cdacc4d030778ab953c39fb7cb571cc2f23f2
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2024-10-17 12:37:10 -0300

    types: implement optional

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,8 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
 import { Contract } from 'ethers'
 
+export type Optional<N> = undefined | N
+
 export type NAry<N> = N | N[]
 
 export type Account = string | Contract | SignerWithAddress
