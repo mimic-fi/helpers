60d16597ba7f473247caa3de0244fd2a99bb4556
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2024-10-17 12:37:39 -0300

    types: implement to address helper

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,5 @@ export type Optional<N> = undefined | N
 export type NAry<N> = N | N[]
 
 export type Account = string | Contract | SignerWithAddress
+
+export const toAddress = (account: Account): string => (typeof account == 'string' ? account : account.address)
